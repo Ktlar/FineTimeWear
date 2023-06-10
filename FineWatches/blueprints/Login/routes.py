1a6ab902af8a61b19ddb54f61dc9690dd0a748dd
Ktlar <95038530+Ktlar@users.noreply.github.com>
2023-06-10 23:14:40 +0200

    Can now run website and can get to home

--- FineWatches/blueprints/Login/routes.py
+++ FineWatches/blueprints/Login/routes.py
@@ -39,25 +39,25 @@ Login = Blueprint('Login', __name__)
 
 
 @Login.route("/")
-@Login.route("/home")
-def home():
-    return render_template('pages/home.html')
+@Login.route("/index")
+def index():
+    return render_template('index.html')
 
 
 @Login.route("/about")
 def about():
-    return render_template('pages/about.html')
+    return render_template('about.html')
 
 
 @Login.route("/style-guide")
 def style_guide():
-    return render_template('pages/style-guide.html')
+    return render_template('style-guide.html')
 
 
 @Login.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('Login.home'))
+        return redirect(url_for('Login.index'))
     form = UserLoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -66,13 +66,13 @@ def login():
                 login_user(user, remember=True)
                 next_page = request.args.get('next')
                 return redirect(next_page) if next_page else redirect(url_for('Login.home'))
-    return render_template('pages/login.html', form=form)
+    return render_template('login.html', form=form)
 
 
 @Login.route("/signup", methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('Login.home'))
+        return redirect(url_for('Login.index'))
     form = UserSignupForm()
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -90,7 +90,7 @@ def signup():
                 login_user(user, remember=True)
                 next_page = request.args.get('next')
                 return redirect(next_page) if next_page else redirect(url_for('Login.home'))
-    return render_template('pages/signup.html', form=form)
+    return render_template('signup.html', form=form)
 
 
 @Login.route("/logout")
